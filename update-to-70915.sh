659eedbb26bf2110607adff142a8293e869863a3
muzoff <muzoff@gmail.com>
2018-12-05 18:18:57 +0200

    Update update-to-70915.sh

--- update-to-70915.sh
+++ update-to-70915.sh
@@ -14,21 +14,21 @@ RPC_PORT=44005
 
 function download_node() {
   echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}."
-  cd $TMP_FOLDER 
+  cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   chmod 755 $COIN_ZIP
-  tar -xvzf $COIN_ZIP 
+  tar -xvzf $COIN_ZIP >/dev/null 2>&1
   cp marcoin* $COIN_PATH
   chmod 755 /usr/local/bin/*
-  cd - >/dev/null 2>&1
-  rm -rf $TMP_FOLDER 
+  cd - >/dev/null 2>&1 >/dev/null 2>&1
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
 }
 
 function update_node() {
   echo -e "Checking if ${RED}$COIN_NAME${NC} is already installed and running the lastest version."
   systemctl daemon-reload
   sleep 3
-  systemctl start $COIN_NAME.service 
+  systemctl start $COIN_NAME.service >/dev/null 2>&1
   sleep 10
   apt -y install jq getinfo 2>/dev/null
   PROTOCOL_VERSION=$($COIN_PATH$COIN_CLI getinfo 2>/dev/null| jq .protocolversion)
@@ -40,10 +40,10 @@ function update_node() {
   elif [[ "$PROTOCOL_VERSION" -le 70914 ]]
   then
     echo -e "You are not running the latest version, sit tight while the update is taking place."
-    systemctl stop $COIN_NAME.service 
-    $COIN_PATH$COIN_CLI stop 
-    sleep 10 
-    rm $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI 
+    systemctl stop $COIN_NAME.service >/dev/null 2>&1
+    $COIN_PATH$COIN_CLI stop >/dev/null 2>&1
+    sleep 10 >/dev/null 2>&1
+    rm $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI >/dev/null 2>&1
     download_node
     configure_systemd
     echo -e "${RED}$COIN_NAME${NC} updated to the latest version. Please make sure the Windows/Mac wallet is also updated."
